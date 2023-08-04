1251777c3a635e650838019c790d3bb74d48b89d
EdwardMelendezM <192666@unsaac.edu.pe>
2023-08-04 17:07:58 -0500

    fix errors

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -8,13 +8,9 @@ const getCurrentUser = async () => {
 
     //Verificamos si existe el usuario de session
     if (!session?.user?.email) {
-      console.log("ERROR Session de usuario")
-      console.log(session)
       return null;
     }
     
-    console.log("Session de usuario")
-    console.log(session)
     //Buscamos el actual usuario
     const currentUser = await prisma.user.findFirst({
       where: {
